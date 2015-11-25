974269ccac9c9df8e2c08446ab99661c5814d51b
w111 <andrey.myhskin@gmail.ocm>
2015-11-25 23:19:29 +0300

    1

---
+++ технрсила.py
@@ -0,0 +1,9 @@
+#import bs4
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get(
+    "http://sankt-peterburg.tehnosila.ru/catalog/tehnika_dlya_kuhni/krupnaja_bytovaja_tehnika/holodilniki/-/27242")
+soup = BeautifulSoup(page.text, "html.parser")
+h1 = soup.body.findAll(['h1'])[0]
+print(h1.text)
