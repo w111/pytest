996015e4e1fe70cc1e35ad948eedf4a7a7593a69
w111 <andrey.myhskin@gmail.ocm>
2015-12-10 22:26:25 +0300

    script

--- requests test.py
+++
@@ -1,9 +0,0 @@
-import requests
-r = requests.get('https://api.github.com', auth=('user', 'pass'))
-print(r.status_code)
-print(r.headers['content-type'])
-print(r.text)
-print(r.json())
-
-r = requests.get('http://gun.io')
-print (r.content)
